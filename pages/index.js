602d42377152de3bb9f1afa6745d3be119676cb2
DumasOlivier <dumas.olivier@outlook.fr>
2022-04-08 23:48:48 +0200

    Finish front & readme

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 import Web3Modal from 'web3modal';
 import { providers, Contract } from 'ethers';
 import { useEffect, useRef, useState } from 'react';
-import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants';
+import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants/index';
 
 export default function Home() {
   // Keep track of whether the user's wallet is connected or not
